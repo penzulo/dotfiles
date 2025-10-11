657758f1d538fd34300cb11c4382ac19936cf222
penzulo <bhargav.deshpande@mitwpu.edu.in>
2025-10-11 18:02:20 +0530

    chore(qtile): Sort processes for convenience.
    Sorted processes in `autostart.sh` for convenience
    and configured nitrogen to apply random wallappers.

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# 1. Core System Services (Authentication, Notifications, Music)
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-picom &
 dunst &
-udiskie --tray --notify &
 mpd &
-nitrogen --restore &
+
+# 2. Visual Setup (Compositor, Wallpaper)
+picom &
+nitrogen --set-scaled --random /usr/share/backgrounds
+
+# 3. Session & Tray Applications (Power Management, Disk Mounting)
+xfce4-power-manager &
+udiskie --tray --notify &
